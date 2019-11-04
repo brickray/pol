dcc3035e18b9025cad7925af9cba74286c681f0a
FuHong <451107390@qq.com>
2019-11-04 22:04:01 +0800

    height field shape

--- src/shape/heightfield.h
+++ src/shape/heightfield.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "../core/shape.h"
+#include "triangle.h"
 
 namespace pol {
-	class HeightField : public Shape {
-
-	};
+	//input:   height map
+	//output:  triangle mesh
+	vector<Triangle*> CreateHeightFieldShape(const Transform& world, int w, int h, const vector<Vector3f>& height, Bsdf* bsdf, Texture* alphaMask = nullptr);
+	vector<Triangle*> CreateHeightFieldShape(const Transform& world, const char* file, Bsdf* bsdf, Texture* alphaMask = nullptr);
 }
